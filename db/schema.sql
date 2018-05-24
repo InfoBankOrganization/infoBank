a392c7fbd03d67bc5e0ae381d0140806a34b6449
anaswardeh <wardeh@un.org>
2018-05-24 19:02:00 -0400

    new files

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+### Schema
+
+CREATE DATABASE info_db;
+USE info_db;
+
+##Create the Table Below
---
+++ models/info.js
@@ -0,0 +1,29 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var info = {
+  all: function(cb) {
+    orm.all("question", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("question", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("question", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("question", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = info;
