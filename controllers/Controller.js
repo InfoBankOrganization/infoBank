ff0765632e2268c937b538be95830ac8608e1172
anaswardeh <wardeh@un.org>
2018-05-24 19:07:16 -0400

    new

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -2,14 +2,14 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/info.js");
+// Import the model (info.js) to use its database functions.
+var info = require("../models/info.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  info.all(function(data) {
     var hbsObject = {
-      cats: data
+      info: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/info", function(req, res) {
-  cat.create([
+  info.create([
     "name", "sleepy"
   ], [
     req.body.name, req.body.sleepy
@@ -32,7 +32,7 @@ router.put("/api/info/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
+  info.update({
     sleepy: req.body.sleepy
   }, condition, function(result) {
     if (result.changedRows == 0) {
@@ -47,7 +47,7 @@ router.put("/api/info/:id", function(req, res) {
 router.delete("/api/info/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  info.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
--- models/info.js
+++ models/info.js
@@ -25,5 +25,5 @@ var info = {
   }
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export the database functions for the controller (Controller.js).
 module.exports = info;
